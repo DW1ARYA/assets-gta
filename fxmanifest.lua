a8553625bd643f012a34a61552a8d15ecebed310
dwiaryasetyono <81942340+DW1ARYA@users.noreply.github.com>
2023-05-06 07:20:41 +0700

    Mercedes G Class, Toyota GR Yaris

---
+++ fxmanifest.lua
@@ -0,0 +1,47 @@
+fx_version 'adamant'
+games 'gta5'
+
+files {
+    'metaassets/**/*.meta',
+    'metaassets/**/*.meta',
+    'metaassets/**/*.meta',
+    'metaassets/**/*.meta',
+    'metaassets/**/*.meta',
+    'audiopack/audioconfig/*.dat151.rel',
+    'audiopack/audioconfig/*.dat54.rel',
+    'audiopack/sfx/**/*.awc',
+}
+
+data_file 'HANDLING_FILE' 'metaassets/**/*.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metaassets/**/*.meta'
+data_file 'CARCOLS_FILE' 'metaassets/**/*.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metaassets/**/*.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'metaassets/**/*.meta'
+data_file 'DLCTEXT_FILE' 'metaassets/**/*.meta'
+
+data_file 'AUDIO_GAMEDATA' 'audiopack/audioconfig/m297zonda_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audiopack/audioconfig/suzukigsxr1k_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audiopack/audioconfig/tayamahar1_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audiopack/audioconfig/m113k_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audiopack/audioconfig/ta023l539_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audiopack/audioconfig/ta103ninjah2r_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audiopack/audioconfig/mercm177_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audiopack/audioconfig/aston59v12_game.dat'
+
+data_file 'AUDIO_SOUNDDATA' 'audiopack/audioconfig/m297zonda_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audiopack/audioconfig/aston59v12_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audiopack/audioconfig/mercm177_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audiopack/audioconfig/ta023l539_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audiopack/audioconfig/ta103ninjah2r_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audiopack/audioconfig/m113k_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audiopack/audioconfig/tayamahar1_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audiopack/audioconfig/suzukigsxr1k_sounds.dat'
+
+data_file 'AUDIO_WAVEPACK' 'audiopack/sfx/dlc_m297zonda'
+data_file 'AUDIO_WAVEPACK' 'audiopack/sfx/dlc_aston59v12'
+data_file 'AUDIO_WAVEPACK' 'audiopack/sfx/dlc_mercm177'
+data_file 'AUDIO_WAVEPACK' 'audiopack/sfx/dlc_ta023l539'
+data_file 'AUDIO_WAVEPACK' 'audiopack/sfx/dlc_ta103ninjah2r'
+data_file 'AUDIO_WAVEPACK' 'audiopack/sfx/dlc_m113k'
+data_file 'AUDIO_WAVEPACK' 'audiopack/sfx/dlc_tayamahar1'
+data_file 'AUDIO_WAVEPACK' 'audiopack/sfx/dlc_suzukigsxr1k'
